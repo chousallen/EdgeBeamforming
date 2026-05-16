fb142a76f91c12ea9f14e5518b2d1257f5aee573
hsianyun <hsianyun2003@gmail.com>
2026-05-16 14:57:28 +0800

    Modify the track code

--- Moduletest/track/cordic.v
+++ Moduletest/track/cordic.v
@@ -1,26 +1,30 @@
 module cordic #(
     parameter  N_ITER = 10,
-    parameter  IW = 10, // Input width (s5.4 format)
+    parameter  IW = 10, // Input width (s6.3 format)
     parameter  OW = 10  // Output width (s2.7 format)
 )(
     input wire clk,
-    input wire rst,
+    input wire rst_n,
     input wire valid_in,
-    input wire signed [IW-1:0] x_in,       // s5.4 format
-    input wire signed [IW-1:0] y_in,       // s5.4 format
+    input wire signed [IW-1:0] x1_in,       // s6.3 format
+    input wire signed [IW-1:0] y1_in,       // s6.3 format
+    input wire signed [IW-1:0] x2_in,       // s6.3 format
+    input wire signed [IW-1:0] y2_in,       // s6
     output reg valid_out,
-    output wire signed [OW-1:0] phase_out    // S2.7 (range +-pi)
+    output wire signed [OW-1:0] phase_out1,    // S2.7 (range +-pi)
+    output wire signed [OW-1:0] phase_out2     // S2.7 (range +-pi)
 );
 
 reg [3:0] iter_r, iter_next;
 reg finish_r, finish_next;
-reg signed [IW+1:0]  x_r, y_r;   // x_r: s7.4, y_r: s7.4
-reg signed [IW+1:0]  x_next, y_next;
-reg signed [OW+2:0] z_r, z_next; // s2.10 format to hold angles up to +-pi
-reg neg_x_r;        // 1 = original x_in was negative
-reg neg_y_orig_r;   // sign of original y_in when neg_x_r=1
+reg signed [IW+1:0]  x1_r, y1_r, x2_r, y2_r;   // x_r: s7.4, y_r: s7.4
+reg signed [IW+1:0]  x1_next, y1_next, x2_next, y2_next;
+reg signed [OW+2:0] z1_r, z2_r; // s2.10 format to hold angles up to +-pi
+reg neg_x1_r, neg_x2_r;        // 1 = original x_in was negative
+reg neg_y1_orig_r, neg_y2_orig_r;   // sign of original y_in when neg_x_r=1
 
-assign phase_out = z_r[OW+2:OW-7]; // Take the top 10 bits of z_r to get s2.7 format
+assign phase_out1 = z1_r[OW+2:OW-7]; // Take the top 10 bits of z_r to get s2.7 format
+assign phase_out2 = z2_r[OW+2:OW-7]; // Take the top 10 bits of z2_r to get s2.7 format
 assign valid_out = finish_r;
 
 // atan_table: Q0.10, Multiply by 2^10 to maintain precision in s2.7 format
@@ -36,8 +40,6 @@ function [9:0] atan_table;
             4'd5:  atan_table = 10'd31;     // atan(2^-5) = atan(0.03125)
             4'd6:  atan_table = 10'd15;     // atan(2^-6) = atan(0.015625)
             4'd7:  atan_table = 10'd7;      // atan(2^-7) = atan(0.0078125)
-            4'd8:  atan_table = 10'd3;      // atan(2^-8) = atan(0.00390625)
-            4'd9:  atan_table = 10'd2;      // atan(2^-9) = atan(0.001953125)
             default: atan_table = 10'd0;
         endcase
     end
@@ -50,14 +52,23 @@ end
 
 always @(*) begin
     // Vectoring mode: drive y toward zero and accumulate phase in ang.
-    if (y_r > 0) begin
-        x_next = x_r + (y_r >>> iter_r);
-        y_next = y_r - (x_r >>> iter_r);
-        z_next = z_r + {{3{1'b0}}, atan_table(iter_r[3:0])};
+    if (y1_r > 0) begin
+        x1_next = x1_r + (y1_r >>> iter_r);
+        y1_next = y1_r - (x1_r >>> iter_r);
+        z1_next = z1_r + {{3{1'b0}}, atan_table(iter_r[3:0])};
     end else begin
-        x_next = x_r - (y_r >>> iter_r);
-        y_next = y_r + (x_r >>> iter_r);
-        z_next = z_r - {{3{1'b0}}, atan_table(iter_r[3:0])};
+        x1_next = x1_r - (y1_r >>> iter_r);
+        y1_next = y1_r + (x1_r >>> iter_r);
+        z1_next = z1_r - {{3{1'b0}}, atan_table(iter_r[3:0])};
+    end
+    if (y2_r > 0) begin
+        x2_next = x2_r + (y2_r >>> iter_r);
+        y2_next = y2_r - (x2_r >>> iter_r);
+        z2_next = z2_r + {{3{1'b0}}, atan_table(iter_r[3:0])};
+    end else begin
+        x2_next = x2_r - (y2_r >>> iter_r);
+        y2_next = y2_r + (x2_r >>> iter_r);
+        z2_next = z2_r - {{3{1'b0}}, atan_table(iter_r[3:0])};
     end
 end
 
@@ -67,34 +78,55 @@ localparam signed [12:0] PI_SCALE = 13'sd3216;
 always @(posedge clk or posedge rst) begin
     if(rst) begin
         iter_r     <= 0;
-        x_r        <= 0;
-        y_r        <= 0;
-        z_r        <= 0;
+        x1_r        <= 0;
+        y1_r        <= 0;
+        z1_r        <= 0;
+        x2_r        <= 0;
+        y2_r        <= 0;
+        z2_r        <= 0;
         finish_r   <= 0;
-        neg_x_r    <= 0;
-        neg_y_orig_r <= 0;
+        neg_x1_r    <= 0;
+        neg_x2_r    <= 0;
+        neg_y1_orig_r <= 0;
+        neg_y2_orig_r <= 0;
     end else if(valid_in) begin
         iter_r <= 0;
         // Pre-rotate by pi if x_in < 0 so CORDIC sees positive x
-        if (x_in[9]) begin
-            x_r          <= -{{2{x_in[9]}}, x_in};    // negate x_in to maintain angle (instead of adding pi, we can just flip the vector)
-            y_r          <= -{{2{y_in[9]}}, y_in};    // negate y_in to maintain angle (instead of adding pi, we can just flip the vector)
-            z_r          <= (y_in[9]) ? -{PI_SCALE} : {PI_SCALE};
-            neg_x_r      <= 1'b1;
-            neg_y_orig_r <= y_in[9]; // save sign of original y_in
+        if (x1_in[9]) begin
+            x1_r          <= -{{2{x1_in[9]}}, x1_in};    // negate x_in to maintain angle (instead of adding pi, we can just flip the vector)
+            y1_r          <= -{{2{y1_in[9]}}, y1_in};    // negate y_in to maintain angle (instead of adding pi, we can just flip the vector)
+            z1_r          <= (y1_in[9]) ? -{PI_SCALE} : {PI_SCALE};
+            neg_x1_r      <= 1'b1;
+            neg_y1_orig_r <= y1_in[9]; // save sign of original y_in
         end else begin
-            x_r          <= {{2{x_in[9]}}, x_in};
-            y_r          <= {{2{y_in[9]}}, y_in};
-            z_r          <= 13'sd0;
-            neg_x_r      <= 1'b0;
-            neg_y_orig_r <= 1'b0;
+            x1_r          <= {{2{x1_in[9]}}, x1_in};
+            y1_r          <= {{2{y1_in[9]}}, y1_in};
+            z1_r          <= 13'sd0;
+            neg_x1_r      <= 1'b0;
+            neg_y1_orig_r <= 1'b0;
+        end
+        if (x2_in[9]) begin
+            x2_r          <= -{{2{x2_in[9]}}, x2_in};    // negate x_in to maintain angle (instead of adding pi, we can just flip the vector)
+            y2_r          <= -{{2{y2_in[9]}}, y2_in};    // negate y_in to maintain angle (instead of adding pi, we can just flip the vector)
+            z2_r          <= (y2_in[9]) ? -{PI_SCALE} : {PI_SCALE};
+            neg_x2_r      <= 1'b1;
+            neg_y2_orig_r <= y2_in[9]; // save sign of original y_in
+        end else begin
+            x2_r          <= {{2{x2_in[9]}}, x2_in};
+            y2_r          <= {{2{y2_in[9]}}, y2_in};
+            z2_r          <= 13'sd0;
+            neg_x2_r      <= 1'b0;
+            neg_y2_orig_r <= 1'b0;
         end
         finish_r <= 0;
     end else if(!finish_r) begin
         iter_r <= iter_next;
-        x_r    <= x_next;
-        y_r    <= y_next;
-        z_r    <= z_next;
+        x1_r    <= x1_next;
+        y1_r    <= y1_next;
+        z1_r    <= z1_next;
+        x2_r    <= x2_next;
+        y2_r    <= y2_next;
+        z2_r    <= z2_next;
         finish_r <= finish_next;
     end
 end
---
+++ Moduletest/track/track.v
@@ -0,0 +1,146 @@
+module track #(
+    parameter N_ITER = 10,
+    parameter IW = 10, // Input width for CORDIC (s5.4 format)
+    parameter PW = 10, // Input width for phase  (s2.7 format)
+    parameter OW = 6  // Output width for CORDIC (s2.7 format)
+)(
+    input wire clk,
+    input wire rst_n,
+    input wire valid_in,
+    input wire [1:0]    channel_in;
+    input wire signed [IW-1:0] i_in, // s5.4 format
+    input wire signed [IW-1:0] q_in, // s5.4 format
+    output reg valid_out,
+    output reg [OW-1:0] angle_out // output phase index (0 to 60 for -60 to 60 degrees)
+);
+
+reg signed [IW:0] L_acc_q_r, L_acc_i_r, R_acc_q_r, R_acc_i_r; // s6.4 format for accumulation
+reg signed [IW:0] L_acc_q_next, L_acc_i_next, R_acc_q_next, R_acc_i_next;
+reg [1:0] channel_r, channel_next;
+reg signed [PW-1:0] L_phase_r, R_phase_r; // s2.7 format for phase
+reg signed [PW-1:0] L_phase_next, R_phase_next; // s2.7 format for phase
+reg signed [PW  :0] phase_diff_r, phase_diff_next; // s3.7 format for phase difference (to hold values up to +-180 degrees)
+reg valid_acc_r, valid_acc_next;
+reg valid_out_r, valid_out_next;
+reg phase_out_r, phase_out_next;
+wire signed [IW-1:0] cordic_x1_in, cordic_y1_in, cordic_x2_in, cordic_y2_in;
+wire signed [PW-1:0] cordic_phase_out1, cordic_phase_out2;
+wire cordic_valid_out;
+
+
+// Control Signal Logic
+always @(*) begin
+    channel_next = (valid_in) ? channel_in : channel_r;
+    valid_acc_next = (channel_in == 2'b11 && valid_in) ? 1'b1 : 1'b0;
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        channel_r <= 2'b00;
+        valid_acc_r <= 1'b0;
+    end else begin
+        channel_r <= channel_next;
+        valid_acc_r <= valid_acc_next;
+    end
+end
+
+// Accumulation Logic
+always @(*) begin
+    // Default to hold current values
+    L_acc_q_next = L_acc_q_r;
+    L_acc_i_next = L_acc_i_r;
+    R_acc_q_next = R_acc_q_r;
+    R_acc_i_next = R_acc_i_r;
+
+    if (valid_in) begin
+        case (channel_in)
+            2'b00: begin // Left channel I
+                L_acc_i_next = {{i_in[IW-1]}, i_in};
+                L_acc_q_next = {{q_in[IW-1]}, q_in};
+            end
+            2'b01: begin // Left channel Q
+                L_acc_i_next = L_acc_i_r + {{i_in[IW-1]}, i_in};
+                L_acc_q_next = L_acc_q_r + {{q_in[IW-1]}, q_in};
+            end
+            2'b10: begin // Right channel I
+                R_acc_i_next = {{i_in[IW-1]}, i_in};
+                R_acc_q_next = {{q_in[IW-1]}, q_in};
+            end
+            2'b11: begin // Right channel Q
+                R_acc_i_next = R_acc_i_r + {{i_in[IW-1]}, i_in};
+                R_acc_q_next = R_acc_q_r + {{q_in[IW-1]}, q_in};
+            end
+        endcase
+    end
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        L_acc_q_r <= 0;
+        L_acc_i_r <= 0;
+        R_acc_q_r <= 0;
+        R_acc_i_r <= 0;
+    end else begin
+        L_acc_q_r <= L_acc_q_next;
+        L_acc_i_r <= L_acc_i_next;
+        R_acc_q_r <= R_acc_q_next;
+        R_acc_i_r <= R_acc_i_next;
+    end
+end
+
+assign cordic_x1_in = L_acc_q_r[IW:1]; // Take the upper IW bits for CORDIC input
+assign cordic_y1_in = L_acc_i_r[IW:1];
+assign cordic_x2_in = R_acc_q_r[IW:1];
+assign cordic_y2_in = R_acc_i_r[IW:1];
+
+// Vectroring CORDIC for Phase Calculation
+cordic #(
+    .IW(IW), // s6.3 format
+    .OW(OW), // s2.7 format
+    .N_ITER(N_ITER)
+) cordic_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .valid_in(valid_acc_r),
+    .x1_in(cordic_x1_in),
+    .y1_in(cordic_y1_in),
+    .x2_in(cordic_x2_in),
+    .y2_in(cordic_y2_in),
+    .phase_out1(cordic_phase_out1),
+    .phase_out2(cordic_phase_out2),
+    .valid_out(cordic_valid_out) // not used, we will use finish_r from CORDIC to set valid_out in this module
+);
+
+// Cordic Output Handling
+always @(*) begin
+    L_phase_next = (cordic_valid_out) ? cordic_phase_out1 : L_phase_r; // s2.7 format
+    R_phase_next = (cordic_valid_out) ? cordic_phase_out2 : R_phase_r; // s2.7 format
+    valid_out_next = cordic_valid_out;
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        L_phase_r <= 0;
+        R_phase_r <= 0;
+        valid_out_r <= 0;
+    end else begin
+        L_phase_r <= L_phase_next;
+        R_phase_r <= R_phase_next;
+        valid_out_r <= valid_out_next;
+    end
+end
+
+localparam K_track = 10'sd458; // CORDIC gain for 10 iterations in s2.7 format (1/K = 0.607252935)
+// Phase difference calculation (s3.7 format to hold values up to +-180 degrees)
+always @(*) begin
+    if (valid_out_r) begin
+        phase_diff_next = {{R_phase_r[PW-1]},R_phase_r} - {{L_phase_r[PW-1]},L_phase_r}; // s3.7 format
+        phase_diff_next = phase_diff_next + 10'sd256; // Wrap around
+    end else begin
+        phase_diff_next = phase_diff_r; // Hold previous value when not valid
+    end
+end
+
+
+
+endmodule
